7d24e992e404a3b7154883ba87b5f7be01406883
nicolaskiprop <yegonicholas@gmail.com>
2021-07-24 21:50:09 +0300

    webdev.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", mobileMenu);
 
-function myFunction(){
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar"){
-        x.className += "responsive";
-    } else {
-        x.className = "navbar";
-    }
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+// Inside the Media Query.
+
+.hamburger.active .bar:nth-child(2) {
+    opacity: 0;
+}
+
+.hamburger.active .bar:nth-child(1) {
+    transform: translateY(8px) rotate(45deg);
+}
+
+.hamburger.active .bar:nth-child(3) {
+    transform: translateY(-8px) rotate(-45deg);
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
